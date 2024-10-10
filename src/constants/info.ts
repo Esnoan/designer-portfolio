e4cd42ab07c61a51cdb25749d4ac1cd1d989edf4
Esnoan <andrewu121@gmail.com>
2024-10-10 11:05:55 -0500

    fixes in contact, add download cv

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -1,4 +1,5 @@
 export const links = {
   linkedin: 'https://www.linkedin.com/in/juan-camilo-urbano/',
-  email: 'mailto:jcamilouc1@gmail.com'
+  email: 'mailto:jcamilouc1@gmail.com',
+  phone: 'tel:+573218563235'
 }
